78b4ac71e52b0da28bf5e5271f9157fc872ea207
wangyu <wy200885@163.com>
2018-01-10 16:04:20 +0800

    add gridgain install and test scripts

---
+++ apps/gridgain/gridgain_1/run_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+CURDIR=$(cd `dirname $0`; pwd)
+
+pushd ${CURDIR}/ansible > /dev/null
+
+ansible-playbook -vvv -i hosts run_test.yml --user=root --extra-vars "ansible_sudo_pass=root"
+
+popd > /dev/nul
